96ad9e9ed0bd4fe54ea0a948170c0e4487c0af76
ahorv <attila_horvath@gmx.ch>
2019-01-09 10:45:13 +0100

    added header info

--- bash/mod.sh
+++ bash/mod.sh
@@ -1,2 +1,2 @@
-sudo chown -R pi:pi /home/pi/python_scripts
-sudo chmod -R 777 /home/pi/python_scripts
+sudo chown -R pi:pi /home/pi/camera_scripts
+sudo chmod -R 777 /home/pi/camera_scripts
--- bash/submod.sh
+++ bash/submod.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-# sets permission to all subdirectories 
+# sets file permission to all subdirectories in camera_scripts
+# use bash submod.sh
+# needs to be located within the same directory as camera_scripts
 
-for d in $(find /home/pi/python_scripts -maxdepth 1 -type d)
+for d in $(find /home/pi/camera_scripts -maxdepth 1 -type d)
 do
-  #Do something, the directory is accessible with $d:
+  # set permission to files
+  # directories are accessible with $d:
   sudo chown -R pi:pi $d
   sudo chmod -R 777 $d
   echo $d
